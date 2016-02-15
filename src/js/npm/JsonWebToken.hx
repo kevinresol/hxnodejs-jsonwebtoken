9db25b9da1d8b391f1d866520dadc032fb5e8b60
kevinresol <kevinresol@gmail.com>
2016-02-15 18:16:19 +0800

    Also accepts Buffer

--- src/js/npm/JsonWebToken.hx
+++ src/js/npm/JsonWebToken.hx
@@ -2,13 +2,17 @@ package js.npm;
 
 import js.Error;
 import js.npm.jsonwebtoken.Options;
+import js.node.Buffer;
+
+import haxe.extern.EitherType;
+
 
 @:jsRequire("jsonwebtoken")
 extern class JsonWebToken 
 {
-	public static function sign<T>(payload:T, secret:String, options:SignOptions, ?callback:String->Void):String;
+	public static function sign<T>(payload:T, secret:EitherType<String,Buffer>, options:SignOptions, ?callback:String->Void):String;
 	
-	public static function verify<T>(token:String, secret:String, ?options:VerifyOptions, ?callback:Error->T->Void):T;
+	public static function verify<T>(token:String, secret:EitherType<String,Buffer>, ?options:VerifyOptions, ?callback:Error->T->Void):T;
 	
 	public static function decode<T>(token:String, ?options:DecodeOptions):T;
 }
